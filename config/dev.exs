e5064e65c34002c55f572287e7f3db0311dc9227
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2018-02-06 07:37:49 -0800

    Config and install

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "phx",
+  password: "elixir",
   database: "gigalixir_getting_started_dev",
   hostname: "localhost",
   pool_size: 10
