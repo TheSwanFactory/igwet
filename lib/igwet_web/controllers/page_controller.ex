77d58fb85a478e78a316de2bf80304c2b8362b36
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2018-02-15 07:51:11 -0800

    Get version from Application (#5)
    since Mix is only available at compile time, and thus not in production

--- lib/igwet_web/controllers/page_controller.ex
+++ lib/igwet_web/controllers/page_controller.ex
@@ -2,11 +2,9 @@ defmodule IgwetWeb.PageController do
   use IgwetWeb, :controller
 
   def index(conn, _params) do
-    project = Igwet.Mixfile.project
-
     conn
     |> assign(:name, "IGWET")
-    |> assign(:version, project[:version])
+    |> assign(:version, Application.spec(:igwet, :vsn))
     |> assign(:current_user, get_session(conn, :current_user))
     |> render("index.html")
   end
