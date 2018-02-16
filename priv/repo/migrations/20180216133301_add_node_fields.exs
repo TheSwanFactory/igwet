903250a95c1cc9fc3203d4827c3b9f5967c575ba
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2018-02-16 06:37:22 -0800

    Extend node
    metadata, address

---
+++ priv/repo/migrations/20180216133301_add_node_fields.exs
@@ -0,0 +1,12 @@
+defmodule Igwet.Repo.Migrations.AddNodeFields do
+  use Ecto.Migration
+
+  def change do
+    alter table(:nodes) do
+      add :url, :string
+      add :date, :timestamp
+      add :meta, :map
+      add :address_id, references(:addresses, on_delete: :nothing)
+    end
+  end
+end
