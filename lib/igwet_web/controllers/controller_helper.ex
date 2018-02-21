f80efb8f2fd232c1476dfddac90027b4ddda41c4
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2018-02-21 04:52:20 -0800

    Check node status in require_admin
    Else return 401 Error

--- lib/igwet_web/controllers/controller_helper.ex
+++ lib/igwet_web/controllers/controller_helper.ex
@@ -18,6 +18,17 @@ defmodule IgwetWeb.ControllerHelper do
 
   def require_admin(conn, params) do
     conn = require_auth(conn, params)
+    user = get_session(conn, :current_user)
+    node = user.node
+    if !node do
+      conn
+      |> put_status(401)
+      |> render(ErrorView, :"401", message: "The email for this login is not associated with an active contact")
+    else if !Node.is_admin(node) do
+      conn
+      |> put_status(401)
+      |> render(ErrorView, :"401", message: "The contact for this login does not have the Administrator privilege necessary to view this page")
+    end
     conn
   end
 end
