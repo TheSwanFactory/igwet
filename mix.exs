1bc3d105c7c5df35786a6bd3ee76532c2d9adf4e
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2018-02-28 05:23:24 -0800

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Igwet.Mixfile do
   def project do
     [
       app: :igwet,
-      version: "0.0.2",
+      version: "0.0.12",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
