3412fdee8442d1ad648da006cd3859bf2342e959
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2018-03-20 03:41:02 -0700

    Fail key test

--- test/igwet/network/factory_test.exs
+++ test/igwet/network/factory_test.exs
@@ -2,6 +2,13 @@ defmodule Igwet.NetworkTest.Factory do
   use Igwet.DataCase
   doctest Igwet.Network.Factory
 
+  alias Igwet.Network
+
   describe "factory" do
+    test "IGWET has its own key" do
+      node = Network.get_first_node_named!("IGWET")
+      assert node.key == "com.igwet"
+    end
+
   end
 end
