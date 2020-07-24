45df3de6d7cebeadcf9e717b3f37880f494a6853
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-07-24 08:14:11 -0700

    alter tables

--- priv/repo/migrations/20200724151000_add_aliases.exs
+++ priv/repo/migrations/20200724151000_add_aliases.exs
@@ -2,6 +2,11 @@ defmodule Igwet.Repo.Migrations.AddAliases do
   use Ecto.Migration
 
   def change do
-
+    alter table(:edges) do
+      add :as, :string
+    end
+    alter table(:nodes) do
+      add :initials, :string
+    end
   end
 end
