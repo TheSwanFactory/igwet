5b1f19a93f02069a8a5e671427794434b0e92912
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-09-07 12:56:17 -0700

    POST /webhook/log_sms

--- lib/igwet/network/sms.ex
+++ lib/igwet/network/sms.ex
@@ -110,7 +110,7 @@ defmodule Igwet.Network.SMS do
       ...> |> SMS.phone2member("+8155551212")
       ...> |> SMS.relay_sms()
       ...> |> Enum.sort()
-      [%{body: "f: Hello, Twirled!", debug: true, from: "+13105555555", to: "+3125551212"}, %{body: "f: Hello, Twirled!", debug: true, from: "+13105555555", to: "+8155551212"}]
+      [%{body: "Hello, Twirled!", debug: true, from: "+13105555555", to: "+3125551212"}, %{body: "Hello, Twirled!", debug: true, from: "+13105555555", to: "+8155551212"}]
   """
 
   def relay_sms(params) do
@@ -131,9 +131,6 @@ defmodule Igwet.Network.SMS do
       "from"
       iex> sender.phone
       params["from"]
-      iex> [head | _tail] = String.split(text, ":")
-      iex> head
-      "f"
   """
 
   def to_nodes(params) do
@@ -144,10 +141,17 @@ defmodule Igwet.Network.SMS do
 
     sender = Network.get_first_node!(:phone, params[@from])
     receiver = Network.get_first_node!(:phone, params[@to])
-    text = Network.get_initials(sender) <> ": " <> params[@body]
+    text = params[@body] #Network.get_initials(sender) <> ": " <>
     chat = Network.get_predicate("chat")
-    msg_key = ".msg+" <> sender.key <> "+" <> params[@msg_id]
-    {:ok, message} = Network.create_node %{name: text, key: msg_key}
+    msg_key = "chat+" <> sender.key <> "+" <> params[@msg_id]
+    {:ok, datetime} = DateTime.now(sender.timezone)
+    {:ok, message} = Network.create_node %{
+      about: inspect(params),
+      date: datetime,
+      key: msg_key,
+      name: text,
+      size: String.length(text),
+    }
     Network.make_edge(message, "type", chat)
     #Network.set_node_in_group(message, receiver)
     Network.make_edge(message, "from", sender)
