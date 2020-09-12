487033cff44e1e772cfa155a3fe2740ea3be5fde
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-09-12 01:00:45 -0700

    InlineRelationships

--- priv/repo/migrations/050_add_parent_to_nodes.exs
+++ priv/repo/migrations/050_add_parent_to_nodes.exs
@@ -1,10 +1,13 @@
-defmodule Igwet.Repo.Migrations.AddParentToNodes do
+defmodule Igwet.Repo.Migrations.InlineRelationships do
   use Ecto.Migration
 
   def change do
     alter table(:nodes) do
       add(:parent_id, references(:nodes, on_delete: :delete_all, type: :uuid))
-      add(:type_id, references(:nodes, on_delete: :delete_all, type: :uuid))
+      add(:type, :string)
+    end
+    alter table(:edges) do
+      add :relation, :string
     end
   end
 end
