2343370b388c5d09d5309b070c4515ebbadf3ceb
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-09-14 09:00:17 -0700

    DataMigrator.collapse_edge

--- lib/igwet/data_migrator.ex
+++ lib/igwet/data_migrator.ex
@@ -3,18 +3,31 @@ defmodule Igwet.DataMigrator do
   alias Igwet.Node
 
   def run do
-    Repo.all from n in Node, where !is_nil(n.meta.parent)
+    from n in Node, where !is_nil(n.meta.parent)
+    |> Repo.all()
     |> Enum.each(&inline_parent/1)
 
-    Repo.all from e in Edge, preload: [:subject, :predicate, :object]
-    |> Enum.each(&inline_type/1)
-    |> Enum.each(&inline_relation/1)
+    from (
+      e in Edge,
+      preload: [:subject, :predicate, :object],
+    )
+    |> Repo.all()
+    |> Enum.each(&collapse_edge/1)
     :ok
   end
 
   defp inline_parent(%Node{} node) do
     node.parent_id = node.meta.parent_id
     Network.update_node node
+  end
+
+  defp collapse_edge(%Edge{} edge) do
+    if (edge.predicate.name == "type") do
+      inline_type(edge)
+      Repo.delete edge
+    else
+      inline_relation(edge)
+    end
   end
 
   defp inline_type(%Edge{} edge) do
@@ -24,6 +37,6 @@ defmodule Igwet.DataMigrator do
 
   defp inline_relation(%Edge{} edge) do
     edge.relation = edge.predicate.name
-    Network.update_edge
+    Network.update_edge edge
   end
 end
