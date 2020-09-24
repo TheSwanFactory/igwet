4bbb15e18eee6a11cd12619a6d660daa46974b35
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-09-23 20:02:13 -0700

    Create data_import_test.exs

---
+++ test/igwet/data_import_test.exs
@@ -0,0 +1,14 @@
+defmodule Igwet.NetworkTest.DataImport do
+  #require Logger
+  use Igwet.DataCase
+  #alias Igwet.DataImport
+  #alias Igwet.Network
+
+  #@test_csv "support/ingest-test.csv"
+
+  describe "read csv" do
+    test "before" do
+      assert is_nil nil
+    end
+  end
+end
