b26c7357f6722eaa36f9de6a8d72a1e98a5be3a4
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-09-23 20:17:29 -0700

    csv_map

--- lib/igwet/data_import.ex
+++ lib/igwet/data_import.ex
@@ -1,12 +1,19 @@
 defmodule Igwet.DataImport do
-  require Logger
-  import Ecto.{Query}
-  alias Igwet.Network
-  alias Igwet.Network.Edge
-  alias Igwet.Network.Node
-  alias Igwet.Repo
+  #require Logger
+  #import Ecto.{Query}
+  #alias Igwet.Network.Edge
+  #alias Igwet.Network.Node
+  #alias Igwet.Repo
 
-  def run do
+  def map_strings_to_atoms(string_key_map) do
+    # https://stackoverflow.com/questions/31990134/how-to-convert-map-keys-from-strings-to-atoms-in-elixir
+    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), val}
+  end
+
+  def csv_map(path) do
+    File.stream!(path)
+    |> CSV.decode!(headers: true)
+    |> Enum.each(&map_strings_to_atoms/1)
   end
 
 end
--- test/igwet/data_import_test.exs
+++ test/igwet/data_import_test.exs
@@ -1,14 +1,19 @@
 defmodule Igwet.NetworkTest.DataImport do
   #require Logger
   use Igwet.DataCase
-  #alias Igwet.DataImport
+  alias Igwet.DataImport
   #alias Igwet.Network
 
-  #@test_csv "support/ingest-test.csv"
+  @test_csv "ingest-test.csv"
+  #@test_dir Application.app_dir(:igwet, "../..")
+  @csv_path Path.absname(@test_csv, "test/support")
 
   describe "read csv" do
-    test "before" do
-      assert is_nil nil
+    test "csv_map" do
+      map = DataImport.csv_map(@csv_path)
+      assert !is_nil map
     end
   end
 end
+
+# /Users/ernest/Developer/igwet/_build/test/lib/igwet/../../support/ingest-test.csv
