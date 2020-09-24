119262ab854eb83eda0c39cf3a7e8e931c58f103
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-09-23 19:58:06 -0700

    stub DataImport

---
+++ lib/igwet/data_import.ex
@@ -0,0 +1,12 @@
+defmodule Igwet.DataImport do
+  require Logger
+  import Ecto.{Query}
+  alias Igwet.Network
+  alias Igwet.Network.Edge
+  alias Igwet.Network.Node
+  alias Igwet.Repo
+
+  def run do
+  end
+
+end
