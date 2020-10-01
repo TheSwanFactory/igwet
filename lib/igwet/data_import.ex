8120d451b50dc1fbf20323a2f2ef91f668176d6d
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2020-10-01 06:42:53 -0700

    create_nodes

--- lib/igwet/data_import.ex
+++ lib/igwet/data_import.ex
@@ -2,17 +2,18 @@ defmodule Igwet.DataImport do
   require Logger
   #import Ecto.{Query}
   #alias Igwet.Network.Edge
-  #alias Igwet.Network.Node
+  alias Igwet.Network
   #alias Igwet.Repo
 
   def map_strings_to_atoms(string_key_map) do
     # https://stackoverflow.com/questions/31990134/how-to-convert-map-keys-from-strings-to-atoms-in-elixir
-    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), val}
+    for {key, val} <- string_key_map, into: %{}, do: {String.downcase(key) |> String.to_atom(), val}
   end
 
-  def create_nodes(attrs) do
-    Logger.warn "create_nodes: #{attrs}"
-    %{}
+  def merge_key(attrs, group) do
+    node = Network.create_node attrs
+    Logger.warn "create_node.node:\n#{inspect(node)}"
+    %{index: attrs.index, parent_index: attrs.parent, node_id: node.id}
   end
 
   def csv_map(path) do
@@ -20,6 +21,15 @@ defmodule Igwet.DataImport do
     File.stream!(path)
     |> CSV.decode!(headers: true)
     |> Enum.map(&map_strings_to_atoms/1)
+  end
+
+  def create_nodes(map_list, group) do
+    for attrs <- map_list do
+      attrs
+      |> merge_key(group)
+      |> Network.create_node
+    end
+    |> link_nodes(group)
   end
 
 end
