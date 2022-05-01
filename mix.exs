f12756aa8ebe8878bcfe5679112703faea0cf4f3
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-04-30 21:38:01 -0700

    v0.4.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Igwet.Mixfile do
   def project do
     [
       app: :igwet,
-      version: "0.4.2",
+      version: "0.4.3",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
