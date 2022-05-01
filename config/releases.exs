9f4f7522a80878cf6f962d94a4f1f4af93a60416
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-05-01 15:55:00 -0700

    replace releases with runtime

--- config/releases.exs
+++
@@ -1,5 +0,0 @@
-import Config
-config :igwet, IgwetWeb.Endpoint,
-  server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
