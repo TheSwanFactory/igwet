70069b7703ed6d4693dd5055fd29b6f4685e0281
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-05-03 21:48:53 -0700

    fleep_test stub

---
+++ lib/igwet/network/fleep.ex
@@ -0,0 +1,10 @@
+defmodule Igwet.Network.Fleep do
+  @moduledoc """
+  Wrappers and helpers for sending and receiving Email messages
+  """
+
+  # require IEx; #IEx.pry
+
+  require Logger
+  alias Igwet.Network
+end
---
+++ test/igwet/network/fleep_test.exs
@@ -0,0 +1,14 @@
+defmodule Igwet.NetworkTest.Fleep do
+  use Igwet.DataCase
+  doctest Igwet.Network.Fleep
+
+  alias Igwet.Network.Fleep
+
+  describe "Fleep" do
+    test "Finch request" do
+      data = Finch.build(:get, "https://hex.pm") |> Finch.request(MyFinch)
+      assert data
+    end
+  end
+  
+end
