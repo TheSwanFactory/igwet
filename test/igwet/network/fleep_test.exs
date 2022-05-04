4d5be4633a9c7ef1551519361a705b40effae075
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-05-03 22:11:55 -0700

    test "Fleep config"

--- test/igwet/network/fleep_test.exs
+++ test/igwet/network/fleep_test.exs
@@ -9,6 +9,12 @@ defmodule Igwet.NetworkTest.Fleep do
       data = Finch.build(:get, "https://hex.pm") |> Finch.request(MyFinch)
       assert data
     end
+    test "Fleep config" do
+      user = Application.get_env(:igwet, Igwet.Network.Fleep)[:username]
+      assert user
+      pw = Application.get_env(:igwet, Igwet.Network.Fleep)[:password]
+      assert pw
+    end
   end
 
 end
