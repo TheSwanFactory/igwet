6cf20f2f58313983a85b4bbdd908be34e580accb
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-05-05 05:38:46 -0700

    stub cache

---
+++ lib/igwet/cache.ex
@@ -0,0 +1,17 @@
+defmodule Igwet.Cache do
+  def create(name) do
+    1
+  end
+
+  def exists(name) do
+    False
+  end
+
+  def set(name, key, value) do
+    value
+  end
+
+  def get(name, key) do
+    key
+  end
+end
